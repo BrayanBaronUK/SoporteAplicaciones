db3641e6f558c76324e52f746a4387410d40e78c
Brayan Baron Amaya <brayan.barona@konradlorenz.edu.co>
2020-02-26 01:07:29 -0500

    cambios de ortografia

--- conexion.php
+++ conexion.php
@@ -1,11 +1,5 @@
 <?php
-<<<<<<< HEAD
 	$user = "STCCCDAT";
 	$pass = "STCCCDAT";
 	$db = "//192.168.232.68:1521/DESSTC4G";
-=======
-	$user = "TIENDA_VIRTUAL";
-	$pass = "VIRTUAL";
-	$db = "//192.168.232.79/ISIS";
->>>>>>> e100fb102959b390f46a56b7f27a502466df9977
 ?>
--- ingreso.php
+++ ingreso.php
@@ -1,23 +1,6 @@
 <?php
-<<<<<<< HEAD
 
 $conex2 = oci_connect("STCCCDAT", "STCCCDAT", "//192.168.232.68:1521/DESSTC4G");
-=======
-//phpinfo();
-$oradbCon = "STC4GDESARROLLO=
-  (DESCRIPTION=
-    (ADDRESS=
-      (PROTOCOL=TCP)
-      (HOST=192.168.232.68)
-      (PORT=1521)
-    )
-    (CONNECT_DATA=
-      (SERVER=dedicated)
-      (SERVICE_NAME=DESSTC4G)
-    )
-  )";
-$conex2 = oci_connect("STCCCDAT", "STCCCDAT", $oradbCon);
->>>>>>> e100fb102959b390f46a56b7f27a502466df9977
 
 
 if (!$conex2) {
@@ -28,7 +11,6 @@ trigger_error(htmlentities($m['message']), E_USER_ERROR);
 $var1 = $_POST["user"];
 $var2 = $_POST["pass"];
   
-<<<<<<< HEAD
 $query   = oci_parse($conex2, 'insert into usuarios_p values (:dato1, :dato2)') or die('Cannot parse query oci');
 OCIBindByName($query, ":dato1", $var1);
 OCIBindByName($query, ":dato2", $var2);
@@ -36,24 +18,4 @@ OCIExecute($query, OCI_DEFAULT);
 
 OCICommit($conex2);
 OCILogoff($conex2);
-=======
-$stmt   = oci_parse($conex2, 'insert into usuarios_p values (:dato1, :dato2)') or die('Cannot parse query oci');
-OCIBindByName($query, ":dato1", $var1);
-OCIBindByName($query, ":dato2", $var2); 
-
-
-
-try{ 
-$result = oci_execute($stmt);
-$fila = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS);
-// echo "<pre>";
-// print_r($fila);
-// echo "</pre>";
-
-$parametros['ESTADO'] = $fila['ESTADO']; //PENDIENTE SERVICIO JOSE Y AICARDY  ->venta rapida2
-}
-catch (Exception $e) {
-    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
-}
->>>>>>> e100fb102959b390f46a56b7f27a502466df9977
 ?>
