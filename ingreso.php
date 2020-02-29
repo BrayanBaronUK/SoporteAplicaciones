c8bba4f6a7f36255e49383027d03fc12bfefca6b
Brayan Baron Amaya <brayan.barona@konradlorenz.edu.co>
2020-02-29 18:31:18 -0500

    arreglos

--- ingreso.php
+++ ingreso.php
@@ -1,23 +1,51 @@
 <?php
-include ("conexion.php");
-	
+include_once ("conexion.php");
 $conex2 = oci_connect($user, $pass, $db);
-
-
 if (!$conex2) {
-$m = oci_error();
-trigger_error(htmlentities($m['message']), E_USER_ERROR);
+    $e = oci_error();
+    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 }
-
-$var1 = $_POST["user"];
-$var2 = $_POST["pass"];
-  
-$query   = oci_parse($conex2, 'select * from USUARIOS_SOPORTE_SEG where USUARIO= :dato1 and CLAVE= :dato2') or die('Cannot parse query oci');
-OCIBindByName($query, ":dato1", $var1);
-OCIBindByName($query, ":dato2", $var2);
-OCIExecute($query, OCI_DEFAULT);
-session_start();
-
-OCICommit($conex2);
-OCILogoff($conex2);
+$mensajeOk=false;
+$mensajeError='El sistema no se encuentra disponible';
+if (isset($_POST['user'],$_POST['pass'])):
+	if ($_POST['user']!=""):
+		if ($_POST['pass']!=""):
+          //  $var1 = $_POST['user'];
+          //  $var2 = $_POST['pass'];
+           // $var1 = $_POST["user"];
+           // $var2 = $_POST["pass"];
+           // $con1="LILIA";
+          //  $con2="AVANTEL";
+            // $con3="B";
+            $query= oci_parse($conex2, 'select * from stcccdat.v4pretem');
+           //$query= oci_parse($conex2, "insert into usuarios_soporte_seg values ('$var1','$var2','$con3')") or die('Cannot parse query oci');
+           // $query= oci_parse($conex2, ("select * from USUARIOS_SOPORTE_SEG where usuario='$var1' and clave='$var2'")) or die('Cannot parse query oci');
+            OCIExecute($query);
+           // OCICommit($conex2);
+           // OCILogoff($conex2);
+            $var3 = oci_num_rows($query);
+            echo ("filaaaaaaaaas: $var3");
+            if (oci_num_rows($query)>0):
+                    echo "entro al if";
+					$mensajeOk=true;
+					$usua=oci_fetch_array($query);
+					session_start();
+					$_SESSION['usuario']=$usua[0];
+					$_SESSION['clave']=$usua[1];
+                    $mensajeError='Logueado correctamente ok.';
+                else:
+                echo "error al if";
+				$mensajeError='Usuario o contraseña incorrecta.';	
+				endif;
+			else:
+				$mensajeError='Contraseña incorrecta.';
+			endif;
+		else:
+			$mensajeError='Usuario no existe ok.';
+	endif;
+else:
+	$mensajeError='Todos los datos son requeridos.';
+endif; 
+$salidaJson=array('respuesta' => $mensajeOk, 'mensaje' => $mensajeError);
+echo json_encode($salidaJson);
 ?>
--- js/login.js
+++ js/login.js
@@ -2,22 +2,24 @@ $(document).ready(function(){
 $("#v_ingreso").click(function(){
 		var usuario=$('#user').val();
 		var clave=$('#pass').val();
-		//console.log(usuario,contrasena);
+		console.log(usuario,clave);
 		$.ajax({
 			type:"POST",
 			dataType:'json',
-			url:'php/ingreso.php',
-			data:{usuario:user, clave:pass},
+			url:'ingreso.php',
+			data:{usuario:usuario, clave:clave},
 			success:function(response){
 				if (response.respuesta==true) {
-				//	$('#mensaje').html(response.mensaje);
+					console.log("buenas 1");
+				$('#mensaje').html(response.mensaje);
 					window.location='index.php';
 
 				}else{
-					//$('#mensaje').html(response.mensaje);
+					console.log("buenas 2");
+				$('#mensaje').html(response.mensaje);
 				}
 			},error:function(){
-				alert('Error general en el sistema por diosoos');
+				alert('Error general en el sistema por dios');
 			}
 		});
 	});
