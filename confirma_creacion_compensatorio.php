eafb497d3abc7f08af806ca334d7a5af5be3d2b8
Brayan Baron Amaya <brayan.barona@konradlorenz.edu.co>
2020-04-28 17:37:29 -0500

    cambios limpieza codigo

--- confirma_creacion_compensatorio.php
+++ confirma_creacion_compensatorio.php
@@ -13,8 +13,7 @@
 <?php
 include_once("conexion.php");
 $conex2 = oci_connect($user, $pass, $db);
-//$filas = json_decode($_POST['dia'], true);
-//$filas = json_decode($_POST['especialista'], true);
+
 
 $cedulas = "";
 $arrCedu = array();
@@ -43,8 +42,7 @@ foreach($_POST as $key => $value) {
       $posicionEsp = array_search($row[0],$arrCedu,false);
       $especSql .= " INTO COMPENSATORIOS (USUARIO, ESPECIALISTA, FECHA_CREADO, FECHA_COMPENSATORIO) VALUES ('$row[1]', '$row[2]', SYSDATE, '$arrDias[$posicionEsp]')";
     }
-   // $especSql = rtrim($especSql, ",");
-
+   
     
   
     $sql = "INSERT ALL $especSql SELECT * FROM DUAL";
@@ -54,24 +52,7 @@ foreach($_POST as $key => $value) {
       $queryf = oci_parse($conex2, $sql);
       oci_execute($queryf);
       oci_commit($conex2);
-    
-//    $sql = "INSERT INTO COMPENSATORIOS SELECT USUARIO, '$v_especialista',$v_fecha_cre,'$v_dia' FROM USUARIOS_SOPORTE WHERE (NOMBRES||' '||APELLIDOS) = '$v_especialista'";
-
-/*
-    $inserciones = 0;
-    foreach ($filas as $fila) {
-        $v_dia   = $fila['v_dia'];
-        $v_especialista   = $fila['v_especialista'];
-        $queryf = oci_parse($conex2, $sql);
-        oci_execute($queryf);
-        oci_commit($conex2);  
-        if($queryf) {
-            $inserciones++;
-        }
-    }
-    
-    echo "Se insertaron $inserciones registros";
-*/
+  
 
       $filas = oci_num_rows($queryf);
 
--- confirma_especialista.php
+++ confirma_especialista.php
@@ -1,70 +1,58 @@
-<script language= "javascript" type="text/javascript">
-function AlertaCreacionEsp(){  
+<script language="javascript" type="text/javascript">
+  function AlertaCreacionEsp() {
     alert('Se ha creado usuario correctamente, verificar tabla de usuarios, clave inicial AVANTEL...!');
-    window.location='Especialista_creacion.php';
-}
-function AlertaCamposVacios(){
-    window.location='Especialista_creacion.php';
-} 
-function TiempoEjecucion(){
-  setTimeout(AlertaCamposVacios(),600000);
-}
+    window.location = 'Especialista_creacion.php';
+  }
+
+  function AlertaCamposVacios() {
+    window.location = 'Especialista_creacion.php';
+  }
+
+  function TiempoEjecucion() {
+    setTimeout(AlertaCamposVacios(), 600000);
+  }
 </script>
 <?php
 
 //echo "ingreso a la insersion";
-include_once ("conexion.php");
+include_once("conexion.php");
 $conex2 = oci_connect($user, $pass, $db);
 
-  $v_names=$_POST["names"];
-  $v_lastname=$_POST["lastname"];
-  $v_cedula=$_POST["cedula"];
-  $v_phone=$_POST["phone"];
-  $v_email=$_POST["email"];
-  $v_cargo=$_POST["cargo"];
-  $v_phone_dotacion=$_POST["phone_dotacion"];
-  $v_ip=$_POST["ip"];
-  $v_fecha_cre ="SYSDATE";
-    
-  $sql ="INSERT INTO USUARIOS_SOPORTE VALUES (SUBSTR('$v_names',1,1)||REPLACE(SUBSTR('$v_lastname',1,7),' ',''),'$v_names','$v_lastname',$v_cedula,$v_phone,'$v_email','$v_cargo',$v_phone_dotacion,'$v_ip',$v_fecha_cre)";
+$v_names = $_POST["names"];
+$v_lastname = $_POST["lastname"];
+$v_cedula = $_POST["cedula"];
+$v_phone = $_POST["phone"];
+$v_email = $_POST["email"];
+$v_cargo = $_POST["cargo"];
+$v_phone_dotacion = $_POST["phone_dotacion"];
+$v_ip = $_POST["ip"];
+$v_fecha_cre = "SYSDATE";
 
-    $queryf= oci_parse($conex2, $sql);
-    oci_execute($queryf);
-    oci_commit($conex2);
+$sql = "INSERT INTO USUARIOS_SOPORTE VALUES (SUBSTR('$v_names',1,1)||REPLACE(SUBSTR('$v_lastname',1,7),' ',''),'$v_names','$v_lastname',$v_cedula,$v_phone,'$v_email','$v_cargo',$v_phone_dotacion,'$v_ip',$v_fecha_cre)";
 
-  $filas = oci_num_rows($queryf);
+$queryf = oci_parse($conex2, $sql);
+oci_execute($queryf);
+oci_commit($conex2);
 
-    if ($filas>0):
+$filas = oci_num_rows($queryf);
 
-      $cons = "INSERT INTO USUARIOS_SOPORTE_SEG VALUES(SUBSTR('$v_names',1,1)||REPLACE(SUBSTR('$v_lastname',1,7),' ',''),'AVANTEL','A')";
-      $queryf= oci_parse($conex2, $cons);
-      oci_execute($queryf);
-      oci_commit($conex2);
+if ($filas > 0) :
 
-        echo "<script>";
-        echo "AlertaCreacionEsp()";
-        echo "</script>";
+  $cons = "INSERT INTO USUARIOS_SOPORTE_SEG VALUES(SUBSTR('$v_names',1,1)||REPLACE(SUBSTR('$v_lastname',1,7),' ',''),'AVANTEL','A')";
+  $queryf = oci_parse($conex2, $cons);
+  oci_execute($queryf);
+  oci_commit($conex2);
 
-    else:
-      echo "<script>";
-      echo "TiempoEjecucion()";
-      echo "</script>";
 
-    //  echo "<script>";
-   //   echo "AlertaCamposVacios()";
-    //  echo "</script>";
+  echo "<script>";
+  echo "AlertaCreacionEsp()";
+  echo "</script>";
 
-    endif;
-    //$sql = "select id_usuario, nom_libro,num_dias from reservado  where id_usuario = $v_cedula and num_dias= $v_num_dias and nom_libro = $v_nom_libro";
-   /* $sql = "SELECT NOMBRES, APELLIDOS, CORREO_ELECTRONICO,FECHA_CREACION FROM USUARIOS_SOPORTE WHERE NOMBRES= $v_names AND APELLIDOS= $v_lastname and CORREO_ELECTRONICO= $v_email";
-    $queryf= oci_parse($conex2, $sql);
-    oci_execute($queryf);
-    oci_commit($conex2);     
+else :
+  echo "<script>";
+  echo "TiempoEjecucion()";
+  echo "</script>";
 
-    while ( $row = oci_fetch_array ($queryf)) {
-    echo "Se ha creado el especialista ".$row["NOMBRES"]."  ".$row["APELLIDOS"]." de correo: ".$row["CORREO_ELECTRONICO"]; //."  con fecha: ".$row["FECHA_CREACION"];
-    }
-    echo"<BR>";
-    oci_execute($queryf);
-    oci_commit($conex2);*/
+endif;
+
 ?>
