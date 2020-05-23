5be0cdcccad2396b99c2a467fd22467652143e41
Brayan Baron Amaya <brayan.barona@konradlorenz.edu.co>
2020-05-23 11:50:27 -0500

    se arregla crea turnos

--- Especialista_creacion.php
+++ Especialista_creacion.php
@@ -12,6 +12,8 @@
 
   <title>Creacion Especialista IT AVANTEL</title>
 
+  <link rel="stylesheet" href="./formularios/Creacion_especialista/css/estilos.css">
+  
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -20,7 +22,7 @@
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
   <!--Estilo de formilario-->
-  <link rel="stylesheet" href="./formularios/Creacion_especialista/css/estilos.css">
+  
   <link rel="stylesheet" href="./formularios/Creacion_especialista/css/font-awesome.css">
 
 </head>
