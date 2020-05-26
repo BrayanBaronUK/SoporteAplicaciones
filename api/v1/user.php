b497a53a0e4c238a92462d96728d50c549fcc621
Omar cardona <omar.cardona@pragma.com.co>
2020-05-26 18:34:48 -0500

    Ajustes finales

--- api/v1/user.php
+++ api/v1/user.php
@@ -14,7 +14,31 @@ switch ($action) {
         }
         $responseObj['data'] = $arrayResults;
         break;
+    case "findByNowTurn": // Encuentra el usuario de turno
+        $currentTime = $_SERVER['REQUEST_TIME'];
+        $currentDate = date('Y-m-d', $currentTime);
 
+        if(($currentTime >= strtotime('22:00:00') && $currentTime <= strtotime('23:59:59'))
+            || ($currentTime >= strtotime('00:00:00') && $currentTime <= strtotime('05:59:59'))) {
+            echo 'N';
+        } else if($currentTime >= strtotime('06:00:00') && $currentTime <= strtotime('13:59:59')) {
+            echo 'M';
+        } else if($currentTime >= strtotime('14:00:00') && $currentTime <= strtotime('21:59:59')) {
+            echo 'T';
+        }
+
+        
+
+        exit();
+
+        $arrayResults = array();
+        if ($result = $mysqli->query("SELECT id, code FROM turns")) {
+            while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                    $arrayResults[] = $row;
+            }
+        }
+        $responseObj['data'] = $arrayResults;
+        break;
     default:
         $responseObj['status'] = 400;
         $responseObj['userMessage'] = "Acción no permitida.";
